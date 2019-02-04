83fff147fd94a2c550fb3b74e8f1f2cc0880e991
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-02-04 12:05:26 -0500

    p1

--- InputData.py
+++ InputData.py
@@ -21,11 +21,6 @@ def get_trans_prob_matrix(trans_matrix):
     # initialize transition probability matrix
     trans_prob_matrix = []
 
-    # for each row in the transition matrix
-    for row in trans_matrix:
-        # calculate the transition probabilities
-        prob_row = np.array(row)/sum(row)
-        # add this row of transition probabilities to the transition probability matrix
-        trans_prob_matrix.append(prob_row)
+
 
     return trans_prob_matrix
