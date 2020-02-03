5959bdb3a0ff254eda7dab5957ac32db8840580f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-03 11:28:14 -0500

    final

--- MarkovModelClasses.py
+++ MarkovModelClasses.py
@@ -2,13 +2,13 @@ import SimPy.RandomVariantGenerators as RVGs
 
 
 class Patient:
-    def __init__(self, id, transition_matrix):
+    def __init__(self, id, transition_prob_matrix):
         """ initiates a patient
         :param id: ID of the patient
-        :param transition_matrix: transition probability matrix
+        :param transition_prob_matrix: transition probability matrix
         """
         self.id = id
-        self.tranProbMatrix = transition_matrix  # transition probability matrix
+        self.tranProbMatrix = transition_prob_matrix  # transition probability matrix
 
 
     def simulate(self, n_time_steps):
--- RunMarkovModel.py
+++ RunMarkovModel.py
@@ -3,7 +3,7 @@ import InputData as D
 
 # create a patient
 myPatient = Cls.Patient(id=1,
-                        transition_matrix=D.get_trans_prob_matrix(D.TRANS_MATRIX))
+                        transition_prob_matrix=D.get_trans_prob_matrix(D.TRANS_MATRIX))
 
 # simulate the patient over the specified time steps
 myPatient.simulate(n_time_steps=D.SIM_TIME_STEPS)
