df86a8a0f45975bb3d4ee498fffe4809e498bfb2
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-04 16:01:50 -0500

    cohort

--- InputData.py
+++ InputData.py
@@ -5,13 +5,6 @@ from enum import Enum
 POP_SIZE = 5000         # cohort population size
 SIM_TIME_STEPS = 100    # length of simulation (years)
 
-# transition matrix
-TRANS_MATRIX = [
-    [1251,  350,    116,    17],   # CD4_200to500
-    [0,     731,    512,    15],   # CD4_200
-    [0,     0,      1312,   437]   # AIDS
-    ]
-
 
 class HealthState(Enum):
     """ health states of patients with HIV """
@@ -19,6 +12,14 @@ class HealthState(Enum):
     CD4_200 = 1
     AIDS = 2
     HIV_DEATH = 3
+
+
+# transition matrix
+TRANS_MATRIX = [
+    [1251, 350, 116, 17],   # CD4_200to500
+    [0, 731, 512, 15],      # CD4_200
+    [0, 0, 1312, 437]       # AIDS
+]
 
 
 def get_trans_prob_matrix(trans_matrix):
